7edea7a6c704c439cf9d45154949ffe209387844
Sezer Teleke <steleke@ekin.com>
2024-06-09 01:38:19 +0300

    P.R. ref added for D.N.

--- erpnext/stock/doctype/delivery_note/delivery_note_dashboard.py
+++ erpnext/stock/doctype/delivery_note/delivery_note_dashboard.py
@@ -8,6 +8,7 @@ def get_data():
 			"Stock Entry": "delivery_note_no",
 			"Quality Inspection": "reference_name",
 			"Auto Repeat": "reference_document",
+			"Purchase Receipt": "inter_company_reference",
 		},
 		"internal_links": {
 			"Sales Order": ["items", "against_sales_order"],
@@ -16,7 +17,7 @@ def get_data():
 			"Purchase Order": ["items", "purchase_order"],
 		},
 		"transactions": [
-			{"label": _("Related"), "items": ["Sales Invoice", "Packing Slip", "Delivery Trip"]},
+			{"label": _("Related"), "items": ["Sales Invoice", "Purchase Receipt", "Packing Slip", "Delivery Trip"]},
 			{"label": _("Reference"), "items": ["Sales Order", "Shipment", "Quality Inspection"]},
 			{"label": _("Returns"), "items": ["Stock Entry"]},
 			{"label": _("Subscription"), "items": ["Auto Repeat"]},
