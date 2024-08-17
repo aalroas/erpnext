78d4108b083161263905750636a8f50fd96e73a5
sezertt <sezertt@gmail.com>
2024-08-17 15:05:41 +0300

    Item dashboard for item list

--- erpnext/public/js/item-dashboard.bundle.js
+++ erpnext/public/js/item-dashboard.bundle.js
@@ -1,4 +1,5 @@
 import "../../stock/dashboard/item_dashboard.html";
+import "../../stock/dashboard/item_dashboard_list_without_add.html";
 import "../../stock/dashboard/item_dashboard_list.html";
 import "../../stock/dashboard/item_dashboard.js";
 import "../../stock/page/warehouse_capacity_summary/warehouse_capacity_summary.html";
